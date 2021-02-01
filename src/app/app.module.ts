87685f144984a1ad4dd5d08cab53682e0dd61733
rbx5 <rbx5.rohit@gmail.com>
2021-02-02 00:51:49 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GiphySearchComponent } from './components/giphy-search/giphy-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GiphySearchComponent
   ],
   imports: [
     BrowserModule,
